f7fd9d58d4f6f5a42a5ec19e273cdcb0f35fb7be
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-11-02 13:57:37 -0300

    creating some basic components

--- GestionDeTurnosFront/src/app/app.routes.ts
+++ GestionDeTurnosFront/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { Hall } from './components/halls/hall/hall';
 
+
+const Lazy = (path: string) =>
+  import(`./features/${path}/${path}.component`).then(m => m.DefaultComponent);
+
 export const routes: Routes = [
-    {path:'',component:Hall}
+      { path: '', redirectTo: 'salas', pathMatch: 'full' },
+  { path: 'salas', loadComponent: () => Lazy('salas') },
+  { path: 'empleados', loadComponent: () => Lazy('empleados') },
+  { path: 'reservas', loadComponent: () => Lazy('reservas') },
+  { path: 'clientes', loadComponent: () => Lazy('clientes') },
+  { path: 'calendario', loadComponent: () => Lazy('calendario') },
+  { path: '**', redirectTo: 'salas' },
 ];
