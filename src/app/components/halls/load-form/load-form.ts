44d7eea9cbf41d7214caec2e8c7a874a5ce75026
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-11-04 13:47:35 -0300

    validate duplicate hall number on create

--- src/app/components/halls/load-form/load-form.ts
+++ src/app/components/halls/load-form/load-form.ts
@@ -16,25 +16,48 @@ export class LoadForm {
   serv = inject(SalasService)
   route = inject(Router)
 
+  //para mostrar error en el html
+  numeroOcupado = false
 
   form = this.fb.group({
-    nombre: ['',[Validators.required,Validators.minLength(3)]],
     numero: [0,[Validators.required,Validators.min(1)]],
     descripcion: ['',[Validators.required,Validators.minLength(6)]],
     cantidad_personas: [0,[Validators.required,Validators.min(1)]],
   });
 
   submit() {
-    if(this.form.invalid) return;
+    if(this.form.invalid) {
+      this.form.markAllAsTouched();
+      return};
 
     const newSala = this.form.getRawValue()
-    this.serv.create(newSala).subscribe({
+
+      this.serv.getAll().subscribe({
+      next: (salas) => {
+        const yaExiste = salas.some(s => s.numero === newSala.numero);
+
+        if (yaExiste) {
+          this.numeroOcupado = true;
+          return;
+        }
+
+
+      this.serv.create(newSala).subscribe({
       next: () => {
         alert('Sala creada con éxito');
         this.route.navigateByUrl('/hall');
       },
       error: (e) => {
         alert('Error al crear la sala');
+        console.error(e);
+      }
+      })
+
+
+  
+    ;
+  },    error: (e) => {
+        alert('Error al verificar número de sala');
         console.error(e);
       }
     });
