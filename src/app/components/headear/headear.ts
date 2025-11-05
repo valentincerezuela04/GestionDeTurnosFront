d0c1c3902f8082566ed5f964fd91c482ae93cc83
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-11-05 17:53:59 -0300

    implementing login and register in header with his respectives directives

--- src/app/components/headear/headear.ts
+++ src/app/components/headear/headear.ts
@@ -1,4 +1,6 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { AuthService } from '../../services/Auth/auth-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-headear',
@@ -8,6 +10,22 @@ import { Component, output } from '@angular/core';
   styleUrl: './headear.css',
 })
 export class Headear {
+  auth = inject(AuthService);
+  private router = inject(Router);
   menuToongle = output<void>()
   onToongle(){this.menuToongle.emit()}
+
+
+  logout(){    
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+
+  goLogin(){
+    this.router.navigate(['/login']);
+  }
+
+  goRegister(){
+    this.router.navigate(['/register']);
+  }
 }
