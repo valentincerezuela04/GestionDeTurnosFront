30c24d15d7b6f3e7d957ffa34cd4b104208a8891
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-11-05 17:33:53 -0300

    implementing register and login in the app

---
+++ src/app/components/login/login-page-component/login-page-component.ts
@@ -0,0 +1,51 @@
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/Auth/auth-service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page-component',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login-page-component.html',
+  styleUrl: './login-page-component.css',
+})
+export class LoginPageComponent {
+    private fb = inject(NonNullableFormBuilder);
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  loading = false;
+  error = '';
+  form = this.fb.group({
+    email: ['',[Validators.required, Validators.email]],
+    contrasena: ['',[Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    this.error = '';
+
+    this.auth.login(this.form.getRawValue()).subscribe({
+      next: () => {
+        this.loading = false;
+        alert('Has iniciado sesión correctamente.');
+        this.router.navigate(['/hall']);
+      },
+      error: (err) => {
+        this.loading = false;
+        this.error = err?.error ?? 'No se pudo iniciar sesión';
+        console.error('Login failed', err);
+      },
+    });
+  }
+
+  hasError(controlName: string, error: string) {
+    const ctrl = this.form.get(controlName);
+    return ctrl?.touched && ctrl.hasError(error);
+  }
+}
--- src/app/components/login/register-page-component/register-page-component.spec.ts
+++ src/app/components/login/register-page-component/register-page-component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Login } from './login';
+import { RegisterPageComponent } from './register-page-component';
 
-describe('Login', () => {
-  let component: Login;
-  let fixture: ComponentFixture<Login>;
+describe('RegisterPageComponent', () => {
+  let component: RegisterPageComponent;
+  let fixture: ComponentFixture<RegisterPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login]
+      imports: [RegisterPageComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Login);
+    fixture = TestBed.createComponent(RegisterPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
