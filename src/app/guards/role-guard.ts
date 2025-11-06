e4da2652d00bce7d812d219ba852a3cbdf89dd96
valentincerezuela04 <valentincerezuela04@gmail.com>
2025-11-06 14:04:55 -0300

    implementing Guards on routes

--- src/app/guards/role-guard.ts
+++ src/app/guards/role-guard.ts
@@ -1,5 +1,27 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AppRole } from '../models/auth.model';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/Auth/auth-service';
 
-export const roleGuard: CanActivateFn = (route, state) => {
+export const roleGuard = (...alowedRoles:AppRole[]): CanActivateFn => (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+      // no logueado
+    if (!authService.isLoggedIn()) {
+      alert('Debes iniciar sesión para acceder a esta página.');
+      return router.createUrlTree(['/login'], {
+        queryParams: { returnUrl: state.url },
+      });
+    }
+
+    // logueado pero sin rol adecuado
+    if (!authService.hasRole(...alowedRoles)) {
+      alert('No tienes permisos para acceder a esta página.');
+      return router.createUrlTree(['/hall']);
+    }
+
+
+    //tofdo bien
   return true;
 };
